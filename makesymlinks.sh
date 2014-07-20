8cd906a490592e4e498dc5cfc53367415d668764
Felippe Nardi <felippe@nardi.me>
2014-07-20 19:31:16 -0400

    Add git dotfiles

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim"    # list of files/folders to symlink in homedir
+files="bashrc vimrc vim gitignore_global gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
